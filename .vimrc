d3ea292f61fb564572ff053173b9715851540c29
Mateu Adsuara <mateuadsuara@gmail.com>
2015-11-01 17:16:45 +0000

    Move setup logic to ruby

--- .vimrc
+++ .vimrc
@@ -69,7 +69,7 @@ let g:airline#extensions#default#layout = [['a', 'b', 'c'], ['x', 'y', 'warning'
 let g:tmuxline_preset = {
       \'win'  : '#I #W',
       \'cwin' : '#I #W',
-      \'y'    : ['#(~/bin/battery)', '%R']}
+      \'y'    : ['#(~/battery)', '%R']}
 
 " Git gutter
 " -----------------------
---
+++ setup.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+
+def symlink(file, destination_folder)
+  filename = File.join(working_dir, file)
+
+  resolved_destination = File.expand_path(destination_folder)
+  sym_link = File.join(resolved_destination, File.basename(filename))
+
+  FileUtils.rm sym_link if File.symlink?(sym_link) || File.exist?(sym_link)
+  FileUtils.ln_s filename, sym_link
+end
+
+def working_dir
+  File.expand_path(File.dirname(__FILE__))
+end
+
+def install_vim_plugin(git_repo)
+  IO.popen("git")
+  repo_name = git_repo.split("/")[1]
+  system("git clone https://github.com/#{git_repo} ~/.vim/bundle/#{repo_name}")
+  system('vim -u NONE -c "Helptags" -c q')
+end
+
+FileUtils.mkdir_p File.expand_path("~/.tmp")
+
+[
+  ".vim",
+  ".vimrc",
+  ".tmux.conf",
+  "battery",
+].each do |file|
+  symlink(file, "~")
+end
+
+[
+  "kien/ctrlp.vim.git",
+  "scrooloose/nerdtree.git",
+
+  "easymotion/vim-easymotion",
+
+  "bling/vim-airline",
+  "edkolev/tmuxline.vim",
+
+  "tomtom/tlib_vim.git",
+  "MarcWeber/vim-addon-mw-utils.git",
+  "garbas/vim-snipmate.git",
+  "honza/vim-snippets.git",
+
+  "airblade/vim-gitgutter.git",
+  "tpope/vim-fugitive.git",
+].each do |plugin|
+  install_vim_plugin(plugin)
+end
