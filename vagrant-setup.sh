40940489ccc0b0e242a8a4fdc8188d263dd3410f
Mateu Adsuara <mateuadsuara@gmail.com>
2018-12-28 14:48:20 +0100

    Install properly JS packages

--- vagrant-setup.sh
+++ vagrant-setup.sh
@@ -1,6 +1,5 @@
 add-apt-repository ppa:tmate.io/archive -y
 apt-get update
-
 apt-get install -y tmate
 ssh-keygen -f /home/vagrant/.ssh/id_rsa -N ''
 
@@ -17,10 +16,14 @@ ruby setup.rb /home/vagrant
 apt-get -y install python3-pip
 pip3 install termtosvg
 
+su vagrant <<'JAVASCRIPT'
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 'lts/*'
 npm install -g elm
 npm install -g elm-test
 npm install -g elm-oracle
 npm install -g elm-format
+JAVASCRIPT
